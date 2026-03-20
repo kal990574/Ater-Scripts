75e6e41a8ac7d0eb106ae8d33e44cc5ed7b233cc
mambasa <101706131+kal990574@users.noreply.github.com>
2026-03-20 18:18:32 +0900

    feat: ServiceLocator 구현

---
+++ Assets/02.Scripts/Core/ServiceLocator.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+namespace _02.Scripts.Core
+{
+    public static class ServiceLocator
+    {
+        private static readonly Dictionary<Type, object> _services = new();
+
+        public static void Register<T>(T service) where T : class
+        {
+            _services[typeof(T)] = service;
+        }
+
+        public static T Get<T>() where T : class
+        {
+            if (_services.TryGetValue(typeof(T), out var service))
+            {
+                return (T)service;
+            }
+            throw new InvalidOperationException($"No service of type {typeof(T)}");
+        }
+
+        public static void Clear()
+        {
+            _services.Clear();
+        }
+    }
+}
