3e93f401b6770960174d1ec05ef911c601331632
mambasa <101706131+kal990574@users.noreply.github.com>
2026-03-20 17:08:20 +0900

    chore: enemykillzone -> deathtrigger 변경

--- Assets/02.Scripts/Enemy/DeathTrigger.cs
+++ Assets/02.Scripts/Enemy/DeathTrigger.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace _02.Scripts.Enemy
 {
-    public class EnemyKillZone : MonoBehaviour
+    public class DeathTrigger : MonoBehaviour
     {
         [SerializeField] private LayerMask _playerLayer;
 
