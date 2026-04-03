92235cc5683d93ff4a4e5cad860e8198a0f4d4b6
packtty18 <packtty18@naver.com>
2026-04-03 17:13:52 +0900

    feat: 메인 점프스케어 베이스 제작
    아직 검증 못함

---
+++ Assets/02.Scripts/02.Ingame/JumpScare/MainJumpScare/Core/MainJumpScareTriggerById.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MainJumpScareTriggerById : MonoBehaviour
+{
+    [Header("Target")]
+    [SerializeField] private string _mainJumpScareId;
+
+    public void Trigger()
+    {
+        if (SubJumpScareManager.Instance == null)
+        {
+            Debug.LogError("JumpScareManager.Instance 가 없어 메인 점프스케어를 실행할 수 없습니다.", this);
+            return;
+        }
+
+        SubJumpScareManager.Instance.ExecuteMainJumpScare(_mainJumpScareId);
+    }
+}
