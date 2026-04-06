8ecfbd102be7c2d9aa6e338b4d17feee68f55296
hyeonjongko <115774307+hyeonjongko@users.noreply.github.com>
2026-04-07 01:30:57 +0900

    feat : Sound Key 등록 완료

--- Assets/02.Scripts/03.Outgame/Sounds/Domain/SoundKey.cs
+++ Assets/02.Scripts/03.Outgame/Sounds/Domain/SoundKey.cs
@@ -26,11 +26,63 @@ public class SoundKey
     public const string QTE_Fail = "QTE_Fail";
 
     //Item
+    public const string Item_Gain = "Item_Gain";
+    public const string Item_Throw = "Item_Throw";
 
+    //Inventory
+    public const string Inventory_Open = "Inventory_Open";
+    public const string Inventory_Close = "Inventory_Close";
+    public const string Inventory_Swap = "Inventory_Swap";
 
+    //Mannequin
+    public const string Mannequin_Squeak_1 = "Mannequin_Squeak_1";
+    public const string Mannequin_Squeak_2 = "Mannequin_Squeak_2";
+    public const string Mannequin_Squeak_3 = "Mannequin_Squeak_3";
+    public const string Mannequin_Squeak_4 = "Mannequin_Squeak_4";
+
+    public const string Manneauin_Walk_1 = "Manneauin_Walk_1";
+    public const string Manneauin_Walk_2 = "Manneauin_Walk_2";
 
     //칠판
     public const string BOARD_OPEN = "Board_Open";
+
+    //자물쇠
+    public const string Lock_TryOpen = "Lock_TryOpen";
+    public const string Lock_Spin = "Lock_Spin";
+    public const string Lock_Fail = "Lock_Fail";
+    public const string Lock_Success = "Lock_Success";
+
+    //문
+    public const string Door_Open = "Door_Open";
+    public const string Door_Close = "Door_Close";
+    public const string Door_Locked = "Door_Locked";
+    public const string Door_Open_Key = "Door_Open_Key";
+
+    //키패드
+    public const string KeyPad_TryOpen = "KeyPad_TryOpen";
+    public const string KeyPad_Button = "KeyPad_Button";
+    public const string KeyPad_Success = "KeyPad_Success";
+    public const string KeyPad_Fail = "KeyPad_Fail";
+
+    //퓨즈
+    public const string Fuse_Connect = "Fuse_Connect";
+    public const string Fuse_All_Connected = "Fuse_All_Connected";
+
+    //서랍
+    public const string Drawer_Locked = "Drawer_Locked";
+    public const string Drawer_Open = "Drawer_Open";
+
+    //금고
+    public const string Safe_TryOpen = "Safe_TryOpen";
+    public const string Safe_Dial = "Safe_Dial";
+    public const string Safe_False = "Safe_False";
+    public const string Safe_Open = "Safe_Open";
+
+    //키카드
+    public const string Keycard_TryOpen = "Keycard_TryOpen";
+    public const string Keycard_Use = "Keycard_Use";
+    public const string Keycard_Open = "Keycard_Open";
+
 
     //----------Stinger----------
 
