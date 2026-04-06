1ad737364cbfe13640349e53a67ff4a24457adf6
hyeonjongko <115774307+hyeonjongko@users.noreply.github.com>
2026-04-06 19:35:51 +0900

    feat : Sound 키 등록중

--- Assets/02.Scripts/03.Outgame/Sounds/Domain/SoundKey.cs
+++ Assets/02.Scripts/03.Outgame/Sounds/Domain/SoundKey.cs
@@ -1,12 +1,38 @@
 public class SoundKey
 {
-    //BGM
+    //----------BGM----------
+    public const string BGM_Title = "BGM_Title";
     public const string BGM_TUTORIAL = "BGM_Tutorial";
+    public const string BGM_Chapter1 = "BGM_Chapter1";
 
-    //SFX
+
+    //----------SFX----------
+    //플레이어
+    public const string Player_Walking = "Player_Walking";
+
+    //라이더 스캔
+    public const string Lidar_Using = "Lidar_Using";
+    public const string Lidar_Empty = "Lidar_Empty";
+    public const string Lidar_Completion = "Lidar_Completion";
+
+    //소나 스캔
+    public const string Sonar_Use = "Sonar_Use";
+    public const string Sonar_CoolTime = "Sonar_CoolTime";
+    public const string Sonar_Empty = "Sonar_Empty";
+
+    //QTE
+    public const string QTE_Enter = "QTE_Enter";
+    public const string QTE_Success = "QTE_Success";
+    public const string QTE_Fail = "QTE_Fail";
+
+    //Item
+
+
+
+    //칠판
     public const string BOARD_OPEN = "Board_Open";
 
-    //stinger
+    //----------Stinger----------
 
 
 }
