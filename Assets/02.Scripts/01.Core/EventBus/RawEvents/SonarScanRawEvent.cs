69d00f1f414d62b3b7dd213706bbc47df8229c95
mambasa <101706131+kal990574@users.noreply.github.com>
2026-04-08 12:36:27 +0900

    feat: sonarscan 윤곽선 추가 효과를 위한 소나 스캔 감지 스크립트 구현

--- Assets/02.Scripts/01.Core/EventBus/RawEvents/SonarScanRawEvent.cs
+++ Assets/02.Scripts/01.Core/EventBus/RawEvents/SonarScanRawEvent.cs
@@ -1,9 +1,27 @@
-﻿public readonly struct SonarScanStartedRawEvent : IGameEvent
+﻿using UnityEngine;
+
+public readonly struct SonarScanStartedRawEvent : IGameEvent
 {
     public GameEventContext Context { get; }
+    public Vector3 Origin { get; }
+    public Vector3 Direction { get; }
+    public float ExpandSpeed { get; }
+    public float ScanRadius { get; }
+    public float ScanAngle { get; }
 
-    public SonarScanStartedRawEvent(GameEventContext context)
+    public SonarScanStartedRawEvent(
+        GameEventContext context,
+        Vector3 origin,
+        Vector3 direction,
+        float expandSpeed,
+        float scanRadius,
+        float scanAngle)
     {
         Context = context;
+        Origin = origin;
+        Direction = direction;
+        ExpandSpeed = expandSpeed;
+        ScanRadius = scanRadius;
+        ScanAngle = scanAngle;
     }
 }
