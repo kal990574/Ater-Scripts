119d57849483f668291f35064fb1fe2ede301a43
packtty18 <packtty18@naver.com>
2026-04-13 09:52:22 +0900

    chore : 폴더 구조 변

--- Assets/02.Scripts/03.Outgame/Achievement/Domain/AchievementAiQuestionUsedEvent.cs
+++
@@ -1,9 +0,0 @@
-public readonly struct AchievementAiQuestionUsedEvent : IAchievementEvent
-{
-    public GameEventContext SourceContext { get; }
-
-    public AchievementAiQuestionUsedEvent(GameEventContext sourceContext)
-    {
-        SourceContext = sourceContext;
-    }
-}
