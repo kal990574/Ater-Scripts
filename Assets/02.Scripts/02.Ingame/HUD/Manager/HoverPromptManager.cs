d87037d227e183babb5177baa4cb65cf9fe65ca0
hyeonjongko <115774307+hyeonjongko@users.noreply.github.com>
2026-04-21 19:02:24 +0900

    chore : 튜토리얼 interact 오브젝트 hover 추가

--- Assets/02.Scripts/02.Ingame/HUD/Manager/HoverPromptManager.cs
+++ Assets/02.Scripts/02.Ingame/HUD/Manager/HoverPromptManager.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public class HoverPromptManager : MonoBehaviour
 {
-    public static HoverPromptManager Instance {  get; private set; }
+    public static HoverPromptManager Instance { get; private set; }
 
     [SerializeField] private HoverPromptDataSO _promptData;
+    [SerializeField] private PlayerController _playerController;
 
     public event Action<string, bool> OnPromptChanged;
+
+    private int[] _cachedIds;
+    private bool _isSuspended;
 
     private void Awake()
     {
@@ -19,14 +23,56 @@ public class HoverPromptManager : MonoBehaviour
         Instance = this;
     }
 
+    private void OnEnable()
+    {
+        if (_playerController == null)
+            _playerController = FindFirstObjectByType<PlayerController>();
+
+        if (_playerController != null)
+        {
+            _playerController.OnPuzzleModeEntered += HandlePuzzleEntered;
+            _playerController.OnPuzzleModeExited += HandlePuzzleExited;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_playerController != null)
+        {
+            _playerController.OnPuzzleModeEntered -= HandlePuzzleEntered;
+            _playerController.OnPuzzleModeExited -= HandlePuzzleExited;
+        }
+    }
+
     public void ShowPrompt(int[] promptIds)
     {
-        string text = _promptData.BuildPromptString(promptIds);
-        OnPromptChanged?.Invoke(text, !string.IsNullOrEmpty(text));
+        _cachedIds = promptIds;
+        if (_isSuspended) return;
+        EmitPrompt(promptIds);
     }
 
     public void HidePrompt()
     {
+        _cachedIds = null;
         OnPromptChanged?.Invoke(string.Empty, false);
+    }
+
+    private void HandlePuzzleEntered(EPuzzleType type)
+    {
+        _isSuspended = true;
+        OnPromptChanged?.Invoke(string.Empty, false);
+    }
+
+    private void HandlePuzzleExited()
+    {
+        _isSuspended = false;
+        if (_cachedIds != null)
+            EmitPrompt(_cachedIds);
+    }
+
+    private void EmitPrompt(int[] promptIds)
+    {
+        string text = _promptData.BuildPromptString(promptIds);
+        OnPromptChanged?.Invoke(text, !string.IsNullOrEmpty(text));
     }
 }
